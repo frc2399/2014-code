4d0400e93eafc011d06546a2087cfee36f7b4a3e
James M. Allen <james.m.allen@gmail.com>
2014-02-13 18:49:31 -0500

    Create AutoPosition command

---
+++ 2014-Robot/src/edu/wpi/first/wpilibj/templates/commands/AutoPosition.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.templates.Vision;
+
+/**
+ *
+ * @author jallen
+ */
+public class AutoPosition extends CommandBase {
+    
+    final double TOLERANCE = 15.0; // acceptable tolerance, in inches
+    
+    double targetDistance;
+    
+    public AutoPosition(double desiredDistance) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        // use Vision.getDistance() to retrieve the current distance in inches
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
