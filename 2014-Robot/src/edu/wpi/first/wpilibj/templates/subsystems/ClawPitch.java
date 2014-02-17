86be82141e4f9540173e8450b0e1441e7e729345
Lauren Dierker <ldierker14@hb.edu>
2014-02-16 23:48:09 -0500

    deleted old pid stuff in claw pitch

--- 2014-Robot/src/edu/wpi/first/wpilibj/templates/subsystems/ClawPitch.java
+++ 2014-Robot/src/edu/wpi/first/wpilibj/templates/subsystems/ClawPitch.java
@@ -25,14 +25,12 @@ public class ClawPitch extends Subsystem {
     public CANJaguar clawPitchMotor;
     // encoder is connected through the CAN
     
-    //CANJaguarPIDActuator pidActuator;
     
     public final double MAX_POSITION;
     public final double MIN_POSITION;
     public final double TOLERANCE;
 
     public ClawPitch() {
-        // super(0, 0, 0);
         MAX_POSITION = 0;
         MIN_POSITION = 0; //TODO get values for these 
         TOLERANCE = 10; // TODO this is the tolerance for position will be changed most likely 
@@ -45,7 +43,7 @@ public class ClawPitch extends Subsystem {
             System.out.println("Adding actuators for ClawPitch to LiveWIndow");
             LiveWindow.addActuator("ClawPitch", "CanJaguarPID", new CANJaguarPIDActuator(clawPitchMotor));
             LiveWindow.addSensor("ClawPitch", "CanJaguarEncoder", new CANJaguarPositionSensor(clawPitchMotor));
-            // LiveWindow.addActuator("ClawPitch", "Motor", clawPitchMotor);
+            //LiveWindow.addActuator("ClawPitch", "Motor", clawPitchMotor);
             //LiveWindow.addActuator("ClawPitch", "PID Controller", getPIDController());
             //LiveWindow.addSensor("ClawPitch", "Encoder", clawPitchEncoder);
         } catch (Exception e) {
